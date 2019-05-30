172692f3f83235d1de0ea1947533e7ef2d9a06ef
Nick <coffeebeforearch@gmail.com>
2019-05-30 10:42:40 -0400

    more plotting changes to improve visability

--- plotting_tools/instrumentation_plot.py
+++ plotting_tools/instrumentation_plot.py
@@ -5,6 +5,7 @@
 from sys import argv
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 # Parses the logs
 # Returns thread_info list with tuples of TID and BBV
@@ -24,6 +25,11 @@ def parse_logs(log, name, warps, warp_info):
         # Remaining lines are BBVs
         else:
             warp_info.append(line)
+
+def fmt(x, pos):
+        a, b = '{:.2e}'.format(x).split('e')
+        b = int(b)
+        return r'${} \times 10^{{{}}}$'.format(a, b)
 
 def main():
     # Get the specific logfile as an argument
@@ -45,17 +51,24 @@ def main():
     # Convert to numpy array
     n_array = np.array(list(m_int_distances)).reshape(warps[0], warps[0])
 
-    name = log.split(".")[0]
+    # Set up the name
+    name = log.split("/")[-1]
+    name = name.split(".")[0]
+
+    # Set up the mask
+    mask = np.tri(n_array.shape[0], k=-1)
+    n_array = np.ma.array(n_array, mask=mask)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     im = ax.imshow(n_array, cmap='hot', interpolation='nearest')
     ax.set_xlabel("Warp i")
     ax.set_ylabel("Warp j")
-    ax.set_title("Basic Block Comparison for kernel_name[0]")
+    ax.set_title("Manhattan Distance for: " + name)
 
-    cbar = fig.colorbar(ax=ax, mappable=im, orientation='horizontal')
+    cbar = fig.colorbar(ax=ax, mappable=im, orientation='vertical', format=matplotlib.ticker.FuncFormatter(fmt))
+    cbar.set_label("Distance between warps")
 
-    plt.show()
+    plt.savefig(name + ".png", dpi=1000)
 
 if __name__ == "__main__":
     main()
