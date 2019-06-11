a75aa78b5704e8832ff7143290fa6283463063a2
Nick <coffeebeforearch@gmail.com>
2019-06-11 15:15:34 -0400

    Parse names and execution times of kernels. Computer % of exec time

--- nvprof_parsing_scripts/nvprof_parser.py
+++ nvprof_parsing_scripts/nvprof_parser.py
@@ -7,3 +7,54 @@ To run:
 
 Output:
 """
+
+import os
+from sys import argv
+
+def process_csv(csv):
+    # Keep track of the application name
+    application_name = csv.split("/")[-1].split(".")[0]
+
+    # Read out all the lines in the CSV file
+    lines = []
+    with open(csv, 'r') as f:
+        lines = f.readlines()
+
+    fields = lines[3].split(",")
+    units = lines[4].split(",")
+
+    exec_time_index = fields.index("\"Duration\"")
+    name_index = fields.index("\"Name\"")
+
+    exec_time_units = units[exec_time_index]
+
+    kernels = []
+    exec_times = []
+    for i in range(5, len(lines)):
+        if "[" not in lines[i]:
+            metrics = lines[i].split(",")
+            kernels.append(metrics[name_index])
+            exec_times.append(float(metrics[exec_time_index]))
+        else:
+            continue
+
+    total_exec_time = sum(exec_times)
+    for i in range(len(kernels)):
+        print(i, kernels[i], exec_times[i] / total_exec_time)
+def main():
+    # Unpack the command line arguments
+    program,directory = argv
+
+    # Walk the directory and find all CSV files
+    csvs = []
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            if ".csv" in name:
+                csvs.append(os.path.join(root, name))
+
+    for csv in csvs:
+        process_csv(csv)
+
+
+if __name__ == "__main__":
+    main()
