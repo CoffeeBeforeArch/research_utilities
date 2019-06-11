c8d55b97c74900ae850b70ce75b961fcf5d46282
Nick <coffeebeforearch@gmail.com>
2019-06-11 17:23:30 -0400

    Some changes for exec time percentage support

--- plotting_tools/instrumentation_plot.py
+++ plotting_tools/instrumentation_plot.py
@@ -32,7 +32,19 @@ def fmt(x, pos):
         b = int(b)
         return r'${} \times 10^{{{}}}$'.format(a, b)
 
-def heatmap(directory):
+def heatmap(directory, exec_time_dir):
+    exec_time_dict = {}
+    app_name = ""
+    for root, dirs, files in os.walk(exec_time_dir):
+        for name in files:
+            if(".csv" in name):
+                app_name = name.split("_")[0]
+                lines = []
+                with open (root + name, "r") as f:
+                    lines = f.readlines()
+
+                exec_time_dict[app_name] = lines
+
     for root, dirs, files in os.walk(directory, topdown=False):
         for name in files:
             if ".txt" in name and "bb_" not in name:
@@ -56,10 +68,16 @@ def heatmap(directory):
                 # Set up the name
                 name = name.split(".")[0]
 
+                # Get the application's name in order to look up exec time
+                app_name = root.split("/")[-2]
+                line_no  =  int(name.split(".")[0].split("_")[-1])
+                exec_lines = exec_time_dict[app_name]
+
                 # Set up the mask, and plot the heatmap
                 mask = np.tri(n_array.shape[0], k=-1)
                 n_array = np.ma.array(n_array, mask=mask)
                 fig = plt.figure()
+                fig.suptitle("Exec % = " + str(float(exec_lines[line_no].split(",")[1]) * 100))
                 ax = fig.add_subplot(111)
                 im = ax.imshow(n_array, cmap='hot', interpolation='nearest')
                 ax.set_xlabel("TB i")
@@ -119,9 +137,9 @@ def histogram(bbv_file):
 
 def main():
     # Get the directories where the logfiles are located
-    script,directory = argv
+    script,log_dir,exec_time_dir = argv
     #histogram(directory)
-    heatmap(directory)
+    heatmap(log_dir, exec_time_dir)
 
 
 if __name__ == "__main__":
