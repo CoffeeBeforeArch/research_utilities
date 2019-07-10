ce36f9fe08abf6e38963c99ff799b5f47fd283e8
Nick <coffeebeforearch@gmail.com>
2019-07-10 14:46:47 -0400

    cleanup of heatmap plotting tool

--- plotting_tools/instrumentation_plot.py
+++ plotting_tools/instrumentation_plot.py
@@ -33,65 +33,44 @@ def fmt(x, pos):
         return r'${} \times 10^{{{}}}$'.format(a, b)
 
 def heatmap(directory):
-    """
-    exec_time_dict = {}
-    app_name = ""
-    for root, dirs, files in os.walk(exec_time_dir):
-        for name in files:
-            if(".csv" in name):
-                app_name = name.split("_")[0]
-                lines = []
-                with open (root + name, "r") as f:
-                    lines = f.readlines()
-
-                exec_time_dict[app_name] = lines
-    """
-
     for root, dirs, files in os.walk(directory, topdown=False):
         for name in files:
             if ".txt" in name and "bb_" not in name:
                 # Empty list that will be populated with thread BB distributions
-                warp_info = []
+                tb_info = []
 
                 # Kernel name and warps
-                warps = []
+                tbs = []
 
                 # Parse the logs
-                print(os.path.join(root,name))
-                parse_logs(os.path.join(root,name), warps, warp_info)
+                parse_logs(os.path.join(root,name), tbs, tb_info)
 
-                # Conver the lines into integers
-                m_distances = warp_info[0].split()
+                # Convert the lines into integers
+                m_distances = tb_info[0].split()
                 m_int_distances = map(int, m_distances)
 
                 # Convert to numpy array
-                n_array = np.array(list(m_int_distances)).reshape(warps[0], warps[0])
+                n_array = np.array(list(m_int_distances)).reshape(tbs[0], tbs[0])
 
                 # Set up the name
                 name = name.split(".")[0]
 
                 # Get the application's name in order to look up exec time
                 app_name = root.split("/")[-2]
-                #line_no  =  int(name.split(".")[0].split("_")[-1])
-                #exec_lines = exec_time_dict[app_name]
 
                 # Set up the mask, and plot the heatmap
                 mask = np.tri(n_array.shape[0], k=-1)
                 n_array = np.ma.array(n_array, mask=mask)
                 fig = plt.figure()
-                #fig.suptitle("Exec % = " + str(float(exec_lines[line_no].split(",")[1]) * 100))
+
                 ax = fig.add_subplot(111)
                 im = ax.imshow(n_array, cmap='hot', interpolation='nearest')
                 ax.set_xlabel("TB i")
                 ax.set_ylabel("TB j")
-                ax.set_title("Manhattan Distance for: " + name)
-
-                # Add the colorbar
-                cbar = fig.colorbar(ax=ax, mappable=im, orientation='vertical', format=matplotlib.ticker.FuncFormatter(fmt))
-                cbar.set_label("Distance between TBs")
+                ax.set_title("Manhattan Distance for: " + app_name)
 
                 # Save the figure
-                plt.savefig(name + ".png", dpi=500)
+                plt.savefig(name + ".pdf")
                 plt.close()
 
 def main():
