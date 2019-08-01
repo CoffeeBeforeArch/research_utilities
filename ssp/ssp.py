432b3391fd62f2d79c8cf48e97a30654090bf1ea
Nick <CoffeeBeforeArch@gmail.com>
2019-08-01 11:53:38 -0400

    Add nvprof parsing, and remove redundant data in other log

--- ssp/ssp.py
+++ ssp/ssp.py
@@ -1,13 +1,66 @@
 # This script implements Spatial SimPoint
 # By Nick from CoffeeBeforeArch
 
+# For command line arguments
 from sys import argv
+# For launching child apps
 import subprocess as sp
+# For clustering and more advanced math
 import numpy as np
+# For making directories
 import os
+# For basic statistics
 import math
-import random
+# For reading config and profiler output
 import yaml
+import csv
+# For random selection from clusters
+import random
+import pprint
+# Read in the nvprof input as a dictionary
+def read_profile(nvprof_file):
+    lines = []
+    with open(nvprof_file, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+# Parse profiling data for TB size, shmem usage, reg usage, etc.
+def parse_profile(profile, grid, block, regs, s_shmem, d_shmem):
+    # Try and avoid bad logs
+    if len(profile) < 3:
+        return
+
+    # Extract the heading and units
+    headings = profile[3].replace("\"", "").split(",")
+    units = profile[4].replace("\"", "").split(",")
+
+    # Pre-calculate the indices for what we are extrating
+    name = headings.index("Name")
+    gx = headings.index("Grid X")
+    gy = headings.index("Grid Y")
+    gz = headings.index("Grid Z")
+    bx = headings.index("Block X")
+    by = headings.index("Block Y")
+    bz = headings.index("Block Z")
+    reg = headings.index("Registers Per Thread")
+    s_shmem_i = headings.index("Static SMem")
+    d_shmem_i = headings.index("Dynamic SMem")
+
+    # Go over the actual lines
+    for i in range(5, len(profile)):
+        # Split the line
+        line = profile[i].replace("\"", "").split(",")
+        if "CUDA " not in line[name]:
+            grid.append([int(line[gx]), int(line[gy]), int(line[gz])])
+            block.append([int(line[bx]), int(line[by]), int(line[bz])])
+            regs.append(int(line[reg]))
+            s_shmem.append((float(line[s_shmem_i]),units[s_shmem_i]))
+            d_shmem.append((float(line[d_shmem_i]),units[d_shmem_i]))
+
+# Calculate max occupancy per-SM
+def max_occupancy(blocks, regs, s_shmem, d_shmem):
+    print(blocks, regs, s_shmem, d_shmem)
+    return
 
 # Read the YAML File in
 def read_yaml(yaml_file):
@@ -17,11 +70,17 @@ def read_yaml(yaml_file):
         yaml_dict = yaml.load(f)
     return yaml_dict
 
-# Launch each app and copy the data
-def launch_app(launch_string, app_dir):
+# Launch each app and copy the basic block logs
+def launch_instrument(launch_string, app_dir):
     p = sp.Popen(launch_string, env=os.environ, shell=True)
     p.wait()
     sp.call("mv bb_log* " + app_dir + "/.", shell=True)
+
+# Launch each app and copy profiler output
+def launch_profile(launch_string, app_dir):
+    p = sp.Popen(launch_string, env=os.environ, shell=True)
+    p.wait()
+    sp.call("mv nvprof.csv " + app_dir + "/.", shell=True)
 
 # Goes through all benchmarks in the YAML file and launches all apps
 # with all set inputs
@@ -36,6 +95,7 @@ def launch_benchmarks(yaml_dict):
         exec_dir = bench_dict["exec_dir"]
         data_dir = bench_dict["data_dirs"]
         launch_arg = bench_dict["param"]
+        profile_arg = bench_dict["profile"]
 
         # Go over all the apps for this benchmark
         for exec_dicts in bench_dict["execs"]:
@@ -45,6 +105,7 @@ def launch_benchmarks(yaml_dict):
 
                 # Make a directory for each app we see if it doesn't exist
                 app_dir = bench + "/" + app
+                print(app_dir)
                 if not os.path.exists(app_dir):
                     os.makedirs(app_dir)
 
@@ -71,10 +132,13 @@ def launch_benchmarks(yaml_dict):
 
                     # Launch the applications (Also copies the logs)
                     launch_string = "LD_PRELOAD=" + launch_arg + " " + app_path + " " + final_arg
-                    launch_app(launch_string, app_dir)
+                    profile_string = profile_arg + " " + app_path + " " + final_arg
+                    launch_instrument(launch_string, app_dir)
+                    launch_profile(profile_string, app_dir)
+
 
 # Helper function to unpack the logfile
-def parse_kernel(kernel, k_name, dim, i_counts, bbs):
+def parse_kernel(kernel, k_name, i_counts, bbs):
     # Read all the lines in
     lines = []
     with open(kernel, 'r') as f:
@@ -83,16 +147,6 @@ def parse_kernel(kernel, k_name, dim, i_counts, bbs):
     # Unpack the data
     i = 0
     k_name.append(lines[i])
-    i += 1
-
-    # Get the kernel dimensions
-    dim.append(int(lines[i]));
-    i += 1
-    dim.append(int(lines[i]));
-    i += 1
-
-    # Get the basic block (remove this probably)
-    bb = int(lines[i])
     i += 1
 
     # Unpack the insns counts and bbv info
@@ -111,20 +165,27 @@ def cluster_advanced(app, logs):
     return
 
 # Clustering function based on exact matches
-def cluster_naive(app, logs):
+def cluster_naive(app, logs, profile):
+    # Unpack the profile
+    lines = read_profile(profile)
+
+    # Get the characteristic from non-instrumented file
+    grids = []
+    blocks = []
+    regs = []
+    s_shmem = []
+    d_shmem = []
+    parse_profile(lines, grids, blocks, regs, s_shmem, d_shmem)
+
     # Unpack each kernel
-    for kernel in logs:
+    for i, kernel in enumerate(logs):
         # Arrays to be filled by parse_kernel
         k_name = []
-        dim = []
         i_counts = []
         bbs = []
 
         # Unpack each kernel
-        parse_kernel(kernel, k_name, dim, i_counts, bbs)
-
-        # Get the total number of TBs
-        num_tbs = dim[0] * dim[1]
+        parse_kernel(kernel, k_name, i_counts, bbs)
 
         # Get total instruction count
         total = sum(i_counts)
@@ -139,11 +200,13 @@ def cluster_naive(app, logs):
 
             # Multiply by number of instructions
             weight = float((len(tmp) * v)) / total
+
             # Append the weighted values and indices
             indices.append(tmp)
             weighted_values.append(weight)
 
         # Calculate max TBs for this kernel
+        max_per_sm = max_occupancy(blocks[i], regs[i], s_shmem[i], d_shmem[i])
 
         # Calculate the number of TBs from each cluster
         tbs_per_cluster = []
@@ -151,21 +214,26 @@ def cluster_naive(app, logs):
             continue
 
 # Find all the log files and create a dict of apps and file paths
-def get_logs(i_dir, app_logs):
+def get_logs(i_dir, app_logs, app_prof):
     # Walk the input directory to find the bb_log files
     for r, d, f in os.walk(i_dir):
         # Each app gets an entry in the dict
         for app in d:
             if app not in app_logs.keys():
                 app_logs[app] = []
+                app_prof[app] = ""
 
-        # Add the individual log paths to the dict
+        # Add the individual log and profile paths to the dict
         for name in f:
             # Hard coded file name for now
             if("bb_log" in name):
                 for app in app_logs.keys():
                     if app in r:
                         app_logs[app].append(r + '/' + name)
+            elif("nvprof" in name):
+                for app in app_prof.keys():
+                    if app in r:
+                        app_prof[app] = r + '/' + name
 
 def main():
     # Read out the yaml file to get executable and data dirs
@@ -180,11 +248,11 @@ def main():
     #if len(argv) == 1:
     #    launch_benchmarks(yaml_dict)
 
-
     for i_dir in i_dirs:
         # Create a dict of app names keys and list of log file value
         app_logs = {}
-        get_logs(i_dir, app_logs)
+        app_prof = {}
+        get_logs(i_dir, app_logs, app_prof)
 
         # Check to see if we already created the output dir on a previous
         # launch. If not, create it.
@@ -195,9 +263,10 @@ def main():
         # Unpack the files
         for app, logs in app_logs.items():
             if "irregular" in i_dir:
+                continue
                 cluster_advanced(app, logs)
             elif "regular" in i_dir:
-                cluster_naive(app, logs)
+                cluster_naive(app, logs, app_prof[app])
 
 if __name__ == "__main__":
     main()
